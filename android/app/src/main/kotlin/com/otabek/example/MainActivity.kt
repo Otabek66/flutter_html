437b8968f8b4df86b1b87382ae477ea3c8bcd005
Otabek66 <otabekotaboev@icloud.com>
2022-07-14 12:32:02 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/otabek/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.otabek.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,158 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:example/screens/home/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share_plus/share_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(
+        title: "Screenshot",
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //Create an instance of ScreenshotController
+  ScreenshotController screenshotController = ScreenshotController();
+
+  @override
+  void initState() {
+    // if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Screenshot(
+              controller: screenshotController,
+              child: Container(
+                  padding: const EdgeInsets.all(30.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blueAccent, width: 5.0),
+                    color: Colors.amberAccent,
+                  ),
+                  child:
+                      const Text("This widget will be captured as an image")),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            ElevatedButton(
+              child: const Text(
+                'Capture Above Widget',
+              ),
+              onPressed: () {
+                screenshotController
+                    .capture(delay: Duration(milliseconds: 10))
+                    .then((capturedImage) async {
+                  ShowCapturedWidget(context, capturedImage!);
+                }).catchError((onError) {
+                  print(onError);
+                });
+              },
+            ),
+            ElevatedButton(
+              child: const Text(
+                'Capture An Invisible Widget',
+              ),
+              onPressed: () async {
+                var container = Container(
+                    padding: const EdgeInsets.all(30.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blueAccent, width: 5.0),
+                      color: Colors.redAccent,
+                    ),
+                    child: Text(
+                      "This is an invisible widget",
+                      style: Theme.of(context).textTheme.headline6,
+                    ));
+                screenshotController
+                    .captureFromWidget(
+                        InheritedTheme.captureAll(
+                            context, Material(child: container)),
+                        delay: Duration(seconds: 1))
+                    .then((capturedImage) {
+                  ShowCapturedWidget(context, capturedImage);
+                });
+                await screenshotController
+                    .capture(delay: const Duration(milliseconds: 10))
+                    .then((Uint8List? image) async {
+                  if (image != null) {
+                    final directory = await getApplicationDocumentsDirectory();
+                    final imagePath =
+                        await File('${directory.path}/image.png').create();
+                    await imagePath.writeAsBytes(image);
+
+                    /// Share Plugin
+                    await Share.shareFiles([imagePath.path]);
+                  }
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<dynamic> ShowCapturedWidget(
+      BuildContext context, Uint8List capturedImage) {
+    return showDialog(
+      useSafeArea: false,
+      context: context,
+      builder: (context) => Center(
+        child: Container(
+            child: capturedImage != null
+                ? Image.memory(capturedImage)
+                : Container()),
+      ),
+    );
+  }
+
+  // _saved(File image) async {
+  //   // final result = await ImageGallerySaver.save(image.readAsBytesSync());
+  //   print("File Saved to Gallery");
+  // }
+}
